7c2d0c827b9384ee1e4578a36da96f4f2ad12039
ruben <r.simons@fz-juelich.de>
2017-07-28 14:16:27 +0200

    Fixed Compilingerror

--- Tecnical Gangplank/Tecnical Gangplank/Logic/Gangplank.cs
+++ Tecnical Gangplank/Tecnical Gangplank/Logic/Gangplank.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using Aimtec;
 using Aimtec.SDK.Damage;
 using Aimtec.SDK.Extensions;
