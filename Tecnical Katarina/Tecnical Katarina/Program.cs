872697c266cdf1fae38089e765a9572dabf89248
Ruben Simons <ruben.simons@web.de>
2017-08-03 20:57:09 +0200

    Tecnical Katarina Initial Release

---
+++ Tecnical Katarina/Tecnical Katarina/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Aimtec;
+using Aimtec.SDK.Events;
+using Aimtec.SDK.Util.Cache;
+using TecnicalKatarina.Configurations;
+
+namespace TecnicalKatarina
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            GameEvents.GameStart += Initialize;
+        }
+
+        private static void Initialize()
+        {
+            if (GameObjects.Player.ChampionName.ToLower() != "katarina")
+            {
+                return;
+            }
+            //Aimtec.SDK.Bootstrap.Load();
+            Storings.ChampionImpl.HandleGameLoad();
+        }
+    }
+}
